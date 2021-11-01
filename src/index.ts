558ca86b7bf86c2eb07c9224b3e7c555e817e9bb
Edgar <edgargc.upc@gmail.com>
2021-11-01 11:34:57 +0100

    full express app

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-import express, { Application, Response } from 'express'
+import app from './config/express'
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
-const app: Application = express()
-
-// Documentation api
-app.use(express.static('public'))
-
-app.get('/user', (_, res: Response) => {
-  res.send('NUWE API is up and running')
+// listen to requests
+const server = app.listen(PORT, () => {
+  return console.log(`server is running on port: ${PORT}`)
 })
 
-app.listen(PORT, () => {
-  return console.log(`server is running on PORT ${PORT}`)
-})
+export default server
