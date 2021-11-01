466f0030aa61b1c73b73f85cc0b28e9d321d6afd
Edgar <edgargc.upc@gmail.com>
2021-11-01 10:47:36 +0100

    add full express api

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,11 +1,36 @@
 import express, { Application, Response } from 'express'
-
+import compress from 'compression'
+import helmet from 'helmet'
+import cors from 'cors'
+import methodOverride from 'method-override'
 import { connect } from './mongo'
 
 // Mongoose connection
 connect()
 
 const app: Application = express()
+
+// Documentation api
+app.use(express.static('public'))
+
+// gzip compression
+app.use(compress())
+
+// lets you use HTTP verbs such as PUT or DELETE
+// in places where the client doesn't support it
+app.use(methodOverride())
+
+// secure apps by setting various HTTP headers
+app.use(helmet())
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+// parse json request body
+app.use(express.json())
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }))
 
 /**
  * @api {test} /test
